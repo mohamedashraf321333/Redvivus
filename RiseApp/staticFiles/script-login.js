41a1a93b02bf4a77cf78a4b9f795fad6837a1f51
mohamedashraf321333 <99046104+mohamedashraf321333@users.noreply.github.com>
2023-01-23 19:11:19 +0200

    Add files via upload

---
+++ RiseApp/staticFiles/script-login.js
@@ -0,0 +1,24 @@
+let prism = document.querySelector(".rec-prism");
+
+function showSignup(){
+  window.location.replace("/sign-up");
+  
+}
+function showLogin(){
+  window.location.replace("/login");
+}
+function showForgotPassword(){
+  window.location.replace("/forget-password");
+}
+
+function showSubscribe(){
+  window.location.replace("/volunteer");
+}
+
+function showContactUs(){
+  window.location.replace("/contact-us");
+}
+
+function showThankYou(){
+  window.location.replace("/");
+}
